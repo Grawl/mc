298e324eedfc40b793505f6414b13d2445560f5a
Daniil Pronin <mail@grawl.ru>
2025-06-19 01:18:40 +1000

    replace mcsrvstat.us with mcstatus.io, add PNG favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang=ts>
-	const title = 'mc.grawl.ru'
+	const host = 'mc.grawl.ru'
+	const title = host
 	const description = 'Личный сервер Minecraft'
-	const status: Promise<{ online: boolean }> = fetch('https://api.mcsrvstat.us/3/mc.grawl.ru')
-		.then(response => response.json())
 </script>
 <svelte:head>
 	<title>{title}</title>
@@ -12,17 +11,18 @@
 	<h1>{title}</h1>
 	<p>{description}</p>
 	<p>
-		{#await status}
-			…
-		{:then response}
-			{#if response.online}
-				<span style='color: green'>online</span>
-			{:else}
-				<span style='color: red'>offline</span>
-			{/if}
-		{/await}
+		<a href="https://t.me/mcgrawlru">Telegram</a>
 	</p>
-	<a href="https://t.me/mcgrawlru">Telegram</a>
+	<p>
+		<a href="https://mcstatus.io/status/java/{host}" target=_blank>
+			<img
+				src="https://api.mcstatus.io/v2/widget/java/{host}?transparent=true&dark=true"
+				alt=status
+				width=860
+				height=240
+			/>
+		</a>
+	</p>
 </section>
 <style lang=stylus>
 .root
@@ -37,10 +37,18 @@
 	background: url('./Enderstone.png') center repeat
 	text-align center
 	text-transform uppercase
-	font-size 2em
+	font-size 1.5em
 	font-weight bold
 	font-family monospace
 	letter-spacing 0.2em
+	@media (min-width 400px)
+		font-size 2em
+p
+	margin-block 0.5em
 a
 	color black
+img
+	max-height 4em
+	width auto
+	background url('./Obsidian.webp') center
 </style>
